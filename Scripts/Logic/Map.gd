b8a0de78669efcc48ca9ca357bbcac4f1f6d5203
Herior <haryawan147@gmail.com>
2024-06-15 19:04:01 +0700

    Robot Deploy Update
    Now Can Deploy Robots !!!

--- Scripts/Logic/Map.gd
+++ Scripts/Logic/Map.gd
@@ -30,6 +30,8 @@ var start = Vector2i(0,0)
 var packedpoints : PackedVector2Array
 @onready var ukuran_jendela = get_window().size + Vector2i(0,23)
 var highlight_zone = []
+var redTeam_deploy_zone = []
+var blueTeam_deploy_zone = []
 
 var Obstacle = load("res://Scenes/Main scenes/obstacles.tscn").instantiate()
 var robots = load("res://Scenes/robot.tscn").instantiate()
@@ -232,11 +234,13 @@ func highlight(robot_pos:Vector2i, type:String ,range:int):
 	#for i in _temp_solid_astargrid :
 		#astar_grid.set_point_solid(i,true)
 
+# Todo : Gridder set on ready
 func _ready():
 	#Ketika script berjalan didalam game
 	if not Engine.is_editor_hint():
 		#set_cell(1,vesorus[0],1,Vector2i(35,3))
 	#
+		$Pergerakan.posisikan_indikator(map_to_local(gridder))
 		robots_manager = get_parent().get_node("robots_manager")
 		battle_manager = get_parent().get_node("battle_manager")
 		obstacle_manager = get_parent().get_node("obstacle_manager")
@@ -265,9 +269,11 @@ func _ready():
 	
 	visible = false
 
+# Todo : show is robot deploying
 func _process(delta):
-	timeinformation.text = "%02d:%02d" % time_to_live()
-	
+	if not Engine.is_editor_hint():
+		timeinformation.text = "%02d:%02d" % time_to_live()
+# Fixme : on Game only run
 		
 func time_to_live():
 	var time_left = turntime.time_left
@@ -275,43 +281,49 @@ func time_to_live():
 	var second = int(time_left) % 60
 	return [minute, second]
 
-	
-func create_robot():
-	#membuat unit
-	var arena_zone = get_used_cells(0)
-	for i in 6 :
-		var found = false
-		for p in arena_zone:
-			if !found:
-				if !astar_grid.is_point_solid(p):
-					if i < 3 :
-						var unit : robot = robots.duplicate()
-						add_child(unit)
-						unit.position = map_to_local(p)
-						unit.get_node("robot_sprite").modulate = "#b1003e"
-						robots_manager.robots["redTeam"]["object"][local_to_map(unit.position)] =  unit
-						robots_manager.robots["redTeam"]["data"][local_to_map(unit.position)] = (
-							robots_manager.robots["redTeam"]["object"][local_to_map(unit.position)].get_robot_datas()
-						)
-						robots_manager.mechAction[unit] = [1,1,1]
-						astar_grid.set_point_solid(p)
-						found = true
-					else :
-						var unit : robot = robots.duplicate()
-						add_child(unit)
-						unit.position = map_to_local(p)
-						unit.get_node("robot_sprite").modulate = "#4c2aed"
-						robots_manager.robots["blueTeam"]["object"][local_to_map(unit.position)] =  unit
-						robots_manager.robots["blueTeam"]["data"][local_to_map(unit.position)] = (
-							robots_manager.robots["blueTeam"]["object"][local_to_map(unit.position)].get_robot_datas()
-						)
-						robots_manager.mechAction[unit] = [1,1,1] # set mechAction in battle manager 
-						astar_grid.set_point_solid(p)
-						found = true
-	#pengumpulan seluruh objek robot menjadi array
-	#					
+#todo : Add cant deploy hints
+#todo : Add low energy hints
+var robot_count = 0
+func create_robot(robotpos : Vector2i):
+	if robot_count < 6:
+		if !astar_grid.is_point_solid(robotpos):
+			if robot_count < 3 :
+				var unit : robot = robots.duplicate()
+				add_child(unit)
+				unit.position = map_to_local(robotpos)
+				unit.get_node("robot_sprite").modulate = "#b1003e"
+				robots_manager.robots["redTeam"]["object"][local_to_map(unit.position)] =  unit
+				robots_manager.robots["redTeam"]["data"][local_to_map(unit.position)] = (
+					robots_manager.robots["redTeam"]["object"][local_to_map(unit.position)].get_robot_datas()
+				)
+				robots_manager.mechAction[unit] = [1,1,1]
+				astar_grid.set_point_solid(robotpos)
+			else :
+				var unit : robot = robots.duplicate()
+				add_child(unit)
+				unit.position = map_to_local(robotpos)
+				unit.get_node("robot_sprite").modulate = "#4c2aed"
+				robots_manager.robots["blueTeam"]["object"][local_to_map(unit.position)] =  unit
+				robots_manager.robots["blueTeam"]["data"][local_to_map(unit.position)] = (
+					robots_manager.robots["blueTeam"]["object"][local_to_map(unit.position)].get_robot_datas()
+				)
+				robots_manager.mechAction[unit] = [1,1,1] # set mechAction in battle manager 
+				astar_grid.set_point_solid(robotpos)
+			robot_count += 1
+			if robot_count < 3 :
+				$Deployzone.position = Vector2i(map_to_local(redTeam_deploy_zone.min())) - (tile_set.cellSize/2)
+				$Deployzone.size = Vector2i(map_to_local(redTeam_deploy_zone.max() - redTeam_deploy_zone.min())) + (tile_set.cellSize/2)
+				$Deployzone.modulate = Color.RED
+			else :
+				$Deployzone.position = Vector2i(map_to_local(blueTeam_deploy_zone.min())) - (tile_set.cellSize/2)
+				$Deployzone.size = Vector2i(map_to_local(blueTeam_deploy_zone.max() - blueTeam_deploy_zone.min())) + (tile_set.cellSize/2)
+				$Deployzone.modulate = Color.BLUE
+	if robot_count == 6:
+		battle_manager.set_battle_state(battle_manager.BattleState.BATTLE)
+		$Deployzone.visible = false
+		$turntime.start()
 
-
+#Fixme obstacle timer is in here
 func create_obstacles_multiplayer():
 	#bagian generate obstacle
 		#generate obstacle in multiplayer
@@ -327,6 +339,7 @@ func create_obstacles_multiplayer():
 			stack.playanimatedspawn()
 			astar_grid.set_point_solid(i,true)
 			await get_tree().create_timer(0.1).timeout
+		_on_obstacle_timer_timeout()
 		
 func _attack_target(action:String,attacker :robot, target):
 	if _helper_selected_action == "attack" :
@@ -353,47 +366,62 @@ func _attack_target(action:String,attacker :robot, target):
 	_helpper_is_play = false
 	_helper_selected_action = ""
 
-
+# Todo filter input : can input only in battle state
 func _input(event):
 	#ketika script berjalan didalam game
 	if not Engine.is_editor_hint():
 		#	#jika input(event) adalah sebuah tombol mouse
 		if event is InputEventMouseButton:
 			if event.is_action_pressed("left-click"):
-				if !_helper_hover and !_helpper_is_play:
-					lets_select_unit()
+				if battle_manager.get_battle_state() == 4:
+					if !_helper_hover and !_helpper_is_play:
+						lets_select_unit()
+				elif battle_manager.get_battle_state() == 3:
+					create_robot(gridder)
 				#tambahan kondisi tombol diatas
 		if event is InputEventMouseMotion:
 			if astar_grid.is_in_boundsv(local_to_map(event.position-position)):
-				gridder = local_to_map(event.position-position)
-				if _helper_selected_action == "move" and _is_action_selected:
-					if highlight_zone.has(gridder):
+				if battle_manager.get_battle_state() == 3:
+					if robot_count < 3:
+						gridder.x = min(max(local_to_map(event.position-position).x, redTeam_deploy_zone.min().x), redTeam_deploy_zone.max().x)
+						gridder.y = min(max(local_to_map(event.position-position).y, redTeam_deploy_zone.min().y), redTeam_deploy_zone.max().y)
 						prerender()
 						$Pergerakan.posisikan_indikator(map_to_local(gridder))
-				if ( 
-					(_helper_selected_action == "attack" and _is_action_selected ) or 
-					(_helper_selected_action == "skill" and _is_action_selected )
-				):
-					if highlight_zone.has(gridder):
+					else:
+						gridder.x = min(max(local_to_map(event.position-position).x, blueTeam_deploy_zone.min().x), blueTeam_deploy_zone.max().x)
+						gridder.y = min(max(local_to_map(event.position-position).y, blueTeam_deploy_zone.min().y), blueTeam_deploy_zone.max().y)
+						prerender()
 						$Pergerakan.posisikan_indikator(map_to_local(gridder))
-				else :
-					$Pergerakan.posisikan_indikator(map_to_local(gridder))
-				
-				#robot information
-				if robots_manager.robots["redTeam"]["object"].has(gridder):
-					var _temp_robot_info = robots_manager.robots["redTeam"]["object"][gridder].get_robot_datas()
-					robot_information.set_info(_temp_robot_info)
-					if get_parent().get_node("Container").visible == false :
-						get_parent().get_node("Container").visible = true
-				elif robots_manager.robots["blueTeam"]["object"].has(gridder) :
-					var _temp_robot_info = robots_manager.robots["blueTeam"]["object"][gridder].get_robot_datas()
-					robot_information.set_info(_temp_robot_info)
-					if get_parent().get_node("Container").visible == false :
-						get_parent().get_node("Container").visible = true
-				else :
-					if get_parent().get_node("Container").visible == true :
-						get_parent().get_node("Container").visible = false
-				
+				else:
+					gridder = local_to_map(event.position-position)
+					if _helper_selected_action == "move" and _is_action_selected:
+						if highlight_zone.has(gridder):
+							prerender()
+							$Pergerakan.posisikan_indikator(map_to_local(gridder))
+					if ( 
+						(_helper_selected_action == "attack" and _is_action_selected ) or 
+						(_helper_selected_action == "skill" and _is_action_selected )
+					):
+						if highlight_zone.has(gridder):
+							$Pergerakan.posisikan_indikator(map_to_local(gridder))
+					else :
+						$Pergerakan.posisikan_indikator(map_to_local(gridder))
+					
+					#Fixme : robot info only on battle
+					#robot information
+					if robots_manager.robots["redTeam"]["object"].has(gridder):
+						var _temp_robot_info = robots_manager.robots["redTeam"]["object"][gridder].get_robot_datas()
+						robot_information.set_info(_temp_robot_info)
+						if get_parent().get_node("Container").visible == false :
+							get_parent().get_node("Container").visible = true
+					elif robots_manager.robots["blueTeam"]["object"].has(gridder) :
+						var _temp_robot_info = robots_manager.robots["blueTeam"]["object"][gridder].get_robot_datas()
+						robot_information.set_info(_temp_robot_info)
+						if get_parent().get_node("Container").visible == false :
+							get_parent().get_node("Container").visible = true
+					else :
+						if get_parent().get_node("Container").visible == true :
+							get_parent().get_node("Container").visible = false
 	#jika input(event) adalah sebuah tombol keyboard
 		if event is InputEventKey:
 	#kalkulasi arah gridder berdasarkan input
@@ -603,26 +631,37 @@ func _on_actionoption_selected_action(value):
 func _on_actionoption_mouse_entered_each_option(condition):
 	_helper_hover = condition
 
-
-
+# Fixme : Deployzone diatur disini
+# todo : nyampur sama first rounds
 func _on_obstacle_timer_timeout():
 	battle_manager.set_battle_state(battle_manager.BattleState.DEPLOYING)
+	for x in tile_set.mapSize.x:
+		for y in tile_set.mapSize.y:
+			if y < 4:
+				redTeam_deploy_zone = redTeam_deploy_zone + [Vector2i(x,y)]
+			elif y > 5:
+				blueTeam_deploy_zone = blueTeam_deploy_zone + [Vector2i(x,y)]
+	
+	$Deployzone.position = Vector2i(map_to_local(redTeam_deploy_zone.min())) - (tile_set.cellSize/2)
+	$Deployzone.size = Vector2i(map_to_local(redTeam_deploy_zone.max() - redTeam_deploy_zone.min())) + (tile_set.cellSize/2)
+	$Deployzone.modulate = Color.RED
+	$Deployzone.visible = true
 	#DEPLOYYYY
-	create_robot()
+	#create_robot()
 	#DEPLOYYYY
-	$turntime.start()
+	#$turntime.start()
 
-
+# todo add battleState == 1 here !!
 func _on_battle_manager_changed_state():
 	if visible == false :
 		visible = true
+		$Deployzone.visible = false
 		$enterarena.start()
 		#+++ animasi masuk game
 
 
 func _on_enterarena_timeout():
 	battle_manager.set_battle_state(battle_manager.BattleState.OBSTACLECREATE)
-	$obstacleTimer.start()
 	create_obstacles_multiplayer()
 
 
@@ -671,15 +710,11 @@ func _on_battle_manager_switched():
 	bluepoint.text = str(battle_manager.turnPoint[1])
 	turned.emit(team)
 
-
+# FIXME : Deleted battle_manager.get_battle_state() == 3
 func _on_button_end_turn_button_down():
-	print(battle_manager.get_battle_state())
 	if battle_manager.get_battle_state() == 4 :
 		_helper_endturn = true
 		$turntime.stop()
 		$turntime.timeout.emit()
 		$turntime.start()
-	if battle_manager.get_battle_state() == 3 :
-		$turntime.stop()
-		$turntime.timeout.emit()
-		$turntime.start()
+		
